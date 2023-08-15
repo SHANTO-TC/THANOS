89a83d140c66bcc8283bada74e6ae95f23a46d8c
SHANTO-TC <142261215+SHANTO-TC@users.noreply.github.com>
2023-08-15 13:06:59 +0600

    Add files via upload

--- THANOS.py
+++ THANOS.py
@@ -325,9 +325,7 @@ def superuser():
         print(logo)
         print("\t\033[1;37m\x1b[41m{ First Get Approvel }\033[00m\033[1;30m")
         print ("")
-        print("" \033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-	 ┃ \033[1;37mNote: That is Paid because 100% Ok ID Just Now Login\033[1;32m ┃
-         ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"")
+        print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ \n┃ \033[1;37mNote: That is Paid because 100% Ok ID Just Now Login\033[1;32m ┃\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
         print ("")
         print(" Your Key is Not Approved ")
         print(" Copy And Send Key To Admin")
